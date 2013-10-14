18c1eec4d3efa224629620e0812817a45438859a
Michael van Holst <mikevanholst@gmail.com>
2013-10-14 17:01:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dysociety::Application.config.secret_key_base = '3c1b55fe303c93a5ced668e758cc3edbd89916f8461c74e0392b3480ad25cb7d37769b1767777244388b0e7c277b555d0614c1659763adb0d8eb6cbacf9a65a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dysociety::Application.config.session_store :cookie_store, key: '_dysociety_session'
