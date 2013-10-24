fdbb7eebffce2d993e65d4a23dac4fc3d39042b9
Michael van Holst <mikevanholst@gmail.com>
2013-10-24 10:59:06 -0400

    adds config.assets.compile=true in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,12 +22,18 @@ Dysociety::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+
+  #this is a fix of the above line for heroku
+  config.assets.compile=true
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
