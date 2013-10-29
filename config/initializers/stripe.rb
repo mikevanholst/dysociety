c9c70d8f9305e0fe494b2351ecd362af6e88760e
Michael van Holst <mikevanholst@gmail.com>
2013-10-29 15:37:10 -0400

    changes for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,9 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['sk_test_ZXmRrGWE6grWtUlQrKCFGadQ'],
-  :secret_key      => ENV['pk_test_e3m9e3RoRew11vPH0hS6Pfkv']
+
+
+  
+  :publishable_key => ENV['pk_test_e3m9e3RoRew11vPH0hS6Pfkv'],
+  :secret_key      => ENV['sk_test_ZXmRrGWE6grWtUlQrKCFGadQ']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
